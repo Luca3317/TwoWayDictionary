e9940c48a998e490625bdf84488ac8a329ca7234
Luca3317 <lweist3317@gmail.com>
2022-10-07 17:21:09 +0200

    first commit

---
+++ Tests/TwoWayDictionaryTests.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoWayDictionary;
+using Xunit;
+
+namespace TwoWayDictionaryTests
+{    
+    public class TwoWayDictionaryTests
+    {
+        TwoWayDictionary<int, string> dict;
+        int[] nums = new int[] { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 99, 100 };
+        string[] strs = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "?", ",", "." };
+
+
+        #region Creation Tests
+
+        [Fact]
+        public void CreateTwoWayDictionary()
+        {
+            dict = new TwoWayDictionary<int, string>();
+            Assert.NotNull(dict);
+        }
+
+        [Fact]
+        public void FailOnSameGenericType()
+        {
+            TwoWayDictionary<int, int> invalidDict = null;
+            try
+            {
+                invalidDict = new TwoWayDictionary<int, int>();
+            }
+            catch (System.TypeInitializationException)
+            {
+                Assert.Null(invalidDict);
+                return;
+            }
+
+            Assert.True(false);
+        }
+
+        #endregion
+
+        #region Adding-only Tests
+
+        [Fact]
+        public void AddElementsTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            for (int i = 0; i < 30; i++)
+                Assert.True(dict.Contains(nums[i]));
+
+            for (int i = 0; i < 30; i++)
+                Assert.True(dict.Contains(strs[i]));
+
+            Assert.True(dict.Count == 30);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void AddDuplicateKeyTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            dict.Add(nums[0], strs[0]);
+
+            bool caughtException = false;
+            try
+            {
+                dict.Add(nums[0], strs[0]);
+            }
+            catch (System.ArgumentException)
+            {
+                caughtException = true;
+            }
+
+            Assert.True(caughtException);
+            VerifyIntegrity<int, string>(dict);
+
+            caughtException = false;
+            try
+            {
+                dict.Add(nums[0], strs[1]);
+            }
+            catch (System.ArgumentException)
+            {
+                caughtException = true;
+            }
+
+            Assert.True(caughtException);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void AddDuplicateValueTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            dict.Add(nums[0], strs[0]);
+
+            bool caughtException = false;
+            try
+            {
+                dict.Add(nums[0], strs[0]);
+            }
+            catch
+            {
+                caughtException = true;
+            }
+
+            if (!caughtException) Assert.True(false);
+            VerifyIntegrity<int, string>(dict);
+
+            caughtException = false;
+            try
+            {
+                dict.Add(nums[1], strs[0]);
+            }
+            catch (System.ArgumentException)
+            {
+                caughtException = true;
+            }
+
+            if (!caughtException) Assert.True(false);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void AddByDirectAccess()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict[nums[i]] = strs[i];
+
+            for (int i = 0; i < 30; i++)
+                Assert.True(dict.Contains(nums[i]));
+
+            for (int i = 0; i < 30; i++)
+                Assert.True(dict.Contains(strs[i]));
+
+            Assert.True(dict.Count == 30);
+            VerifyIntegrity<int, string>(dict);
+        }
+        #endregion
+
+        #region Remove Tests
+
+        [Fact]
+        public void RemoveElementsByKeyTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            for (int i = 0; i < 30; i += 2)
+                dict.Remove(nums[i]);
+
+            for (int i = 1; i < 30; i += 2)
+                Assert.True(dict.Contains(nums[i]));
+
+            for (int i = 1; i < 30; i += 2)
+                Assert.True(dict.Contains(strs[i]));
+
+            Assert.True(dict.Count == 15);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void RemoveElementsByValueTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            for (int i = 0; i < 30; i += 2)
+                dict.Remove(strs[i]);
+
+            for (int i = 1; i < 30; i += 2)
+                Assert.True(dict.Contains(nums[i]));
+
+            for (int i = 1; i < 30; i += 2)
+                Assert.True(dict.Contains(strs[i]));
+
+            Assert.True(dict.Count == 15);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        #endregion
+
+        #region Modify Test
+
+        [Fact]
+        public void ChangeValueTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            dict[nums[0]] = "test";
+
+            Assert.False(dict.Contains(strs[0]));
+            Assert.Equal(dict[nums[0]], "test");
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void FailChangeValueTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            bool caughtException = false;
+            try
+            {
+                dict[nums[0]] = "b";
+            }
+            catch (System.Exception)
+            {
+                caughtException = true;
+            }
+
+            Assert.True(caughtException);
+            Assert.Equal(dict[nums[0]], strs[0]);
+            VerifyIntegrity<int, string>(dict);
+
+            caughtException = false;
+            try
+            {
+                dict[nums[0]] = null;
+            }
+            catch (System.Exception)
+            {
+                caughtException = true;
+            }
+
+            Assert.True(caughtException);
+            Assert.Equal(dict[nums[0]], strs[0]);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void ChangeKeyTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            dict[strs[0]] = 9999;
+
+            Assert.False(dict.Contains(nums[0]));
+            Assert.Equal(dict[strs[0]], 9999);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void FailChangeKeyTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            bool caughtException = false;
+            try
+            {
+                dict[strs[0]] = 10;
+            }
+            catch (System.Exception)
+            {
+                caughtException = true;
+            }
+
+            Assert.True(caughtException);
+            Assert.Equal(dict[strs[0]], nums[0]);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+
+        [Fact]
+        public void OverwriteKeyTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            dict.Overwrite(nums[5], "test", dict[nums[5]]);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        [Fact]
+        public void OverwriteValueTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            for (int i = 0; i < 30; i++)
+                dict.Add(nums[i], strs[i]);
+
+            dict.Overwrite(dict[nums[5]], 9999, nums[5]);
+            VerifyIntegrity<int, string>(dict);
+        }
+
+        #endregion
+
+        [Fact]
+        public void ContainsTest()
+        {
+            dict = new TwoWayDictionary<int, string>();
+
+            dict.Add(nums[0], strs[0]);
+            Assert.True(dict.Contains(nums[0]) && dict.Contains(strs[0]));
+
+            try
+            {
+                dict.Contains(null);
+            }
+            catch (System.ArgumentException)
+            {
+                return;
+            }
+
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void UseCaseTest()
+        {
+            TwoWayDictionary<TestClass1, TestClass2> myDict = new TwoWayDictionary<TestClass1, TestClass2>();
+
+            List<TestClass1> classes1 = new List<TestClass1>();
+            List<TestClass2> classes2 = new List<TestClass2>();
+
+            for (int i = 0; i < 80; i++)
+            {
+                classes1.Add(new TestClass1());
+                classes2.Add(new TestClass2());
+
+                myDict.Add(classes1[i], classes2[i]);
+            }
+
+            VerifyIntegrity<TestClass1, TestClass2>(myDict);
+
+            bool exceptionCaught = false;
+            try
+            {
+                myDict[classes1[23]] = classes2[9];
+            }
+            catch (System.ArgumentException)
+            {
+                exceptionCaught = true;
+            }
+
+            Assert.True(exceptionCaught);
+            VerifyIntegrity<TestClass1, TestClass2>(myDict);
+
+
+            exceptionCaught = false;
+            try
+            {
+                myDict[classes2[12]] = classes1[33];
+            }
+            catch (System.ArgumentException)
+            {
+                exceptionCaught = true;
+            }
+
+            Assert.True(exceptionCaught);
+            VerifyIntegrity<TestClass1, TestClass2>(myDict);
+
+            myDict.Remove(classes1[50]);
+            Assert.False(myDict.Contains(classes1[50]));
+            Assert.True(myDict.Count == 79);
+
+            myDict.Remove(classes2[20]);
+            Assert.False(myDict.Contains(classes2[20]));
+            Assert.True(myDict.Count == 78);
+
+            exceptionCaught = false;
+            try
+            {
+                myDict.Remove(classes2[20]);
+            }
+            catch (System.Exception)
+            {
+                exceptionCaught = true;
+            }
+            Assert.True(exceptionCaught);
+            Assert.False(myDict.Contains(classes2[20]));
+            Assert.True(myDict.Count == 78);
+
+            exceptionCaught = false;
+            try
+            {
+                myDict.Remove(new TestClass1());
+            }
+            catch (System.Exception)
+            {
+                exceptionCaught = true;
+            }
+            Assert.True(exceptionCaught);
+            Assert.True(myDict.Count == 78);
+
+            myDict.Add(classes1[50], classes2[20]);
+            Assert.True(myDict.Contains(classes1[50]) && myDict.Contains(classes2[20]) && myDict[classes1[50]] == classes2[20] && myDict.Count == 79);
+            VerifyIntegrity<TestClass1, TestClass2>(myDict);
+
+            myDict.Overwrite(classes1[12], new TestClass2(), myDict[classes1[12]]);
+            VerifyIntegrity<TestClass1, TestClass2>(myDict);
+
+            myDict.Overwrite(classes2[40], new TestClass1(), myDict[classes2[40]]);
+            VerifyIntegrity<TestClass1, TestClass2>(myDict);
+        }
+
+        private class TestClass1
+        {
+            public int a;
+            public int b;
+        }
+        private class TestClass2 : TestClass1
+        {
+            public string s;
+        }
+
+        /// <summary>
+        /// Verifies whether the dictionary is integer at the moment this function is called.
+        /// See the class invariant in TwoWayDictionary.cs
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="dict"></param>
+        void VerifyIntegrity<T1, T2>(TwoWayDictionary<T1, T2> dict)
+        {
+            // Check if dictionary is initialized
+            Assert.NotNull(dict);
+
+            // Check if the count if the dictionary, forward and backward hold the same amount of keys
+            int count = dict.Count;
+            Assert.Equal(count, dict.Forward.Count);
+            Assert.Equal(count, dict.Backward.Count);
+
+            IReadOnlyCollection<T1> forwKeys = dict.Forward.Keys;
+            IReadOnlyCollection<T2> backwKeys = dict.Backward.Keys;
+            IReadOnlyCollection<T2> forwValues = dict.Forward.Values;
+            IReadOnlyCollection<T1> backWValues = dict.Backward.Values;
+
+            // Check if the forward keys are equal to the backwards values, and vice versa
+            Assert.Equal(forwKeys, backWValues);
+            Assert.Equal(backwKeys, forwValues);
+
+            foreach (var elem in forwKeys) Assert.True(dict.Contains(elem));
+            foreach (var elem in backwKeys) Assert.True(dict.Contains(elem));
+            foreach (var elem in forwValues) Assert.True(dict.Contains(elem));
+            foreach (var elem in backWValues) Assert.True(dict.Contains(elem));
+        }
+    }
+}
---
+++ TwoWayDictionary.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoWayDictionary
+{
+    /// <summary>
+    /// <para>
+    /// A bi-directional dictionary.
+    /// Every value in the TwoWayDictionary serves both as key and as value (bijective).
+    /// <paramref name="T1"/> and <paramref name="T2"/> are required to be of different types; otherwise the constructor will throw an exception.
+    /// </para>
+    ///
+    /// <para>
+    /// Warning: Not threadsafe!
+    /// Maybe implement later using Double Buffer?
+    /// </para>
+    /// 
+    /// <para>
+    /// Class invariant:
+    /// At any point in time:
+    /// <list type="table">
+    ///    <item> For every key x mapped to value y in forward, there is a key y mapped to value x in backward </item>
+    ///    <item> For every key x mapped to value y in backward, there is a key y mapped to value x in backward </item>
+    /// </list>
+    /// </para>
+    /// </summary>
+    /// <typeparam name="T1">Type used as key in forward / value in backward. Must be a different type from T2</typeparam>
+    /// <typeparam name="T2">Type used as value in forward / key in backward. Must be a different type from T1</typeparam>
+
+    public class TwoWayDictionary<T1, T2>
+    {
+        readonly Dictionary<T1, T2> forward = new Dictionary<T1, T2>();
+        readonly Dictionary<T2, T1> backward = new Dictionary<T2, T1>();
+
+        public Exposer<T1, T2> Forward { get; private set; }
+        public Exposer<T2, T1> Backward { get; private set; }
+
+        public TwoWayDictionary()
+        {
+            if (typeof(T1) == typeof(T2)) throw new TypeInitializationException("Generics of same type", new Exception("TwoWayDictionary requires its two generic type parameters to be of different types"));
+
+            Forward = new Exposer<T1, T2>(forward);
+            Backward = new Exposer<T2, T1>(backward);
+        }
+
+        public int Count => forward.Count;
+
+        public bool Contains(T1 t1) => forward.ContainsKey(t1);
+        public bool Contains(T2 t2) => backward.ContainsKey(t2);
+
+        public T2 this[T1 index]
+        {
+            get { return forward[index]; }
+            set
+            {
+                if (Contains(value)) throw new ArgumentException("The given value is already in the dictionary; TwoWayDictionary allows only 1:1 connections, therefore each value may only be added once, whether as key or as value");
+                if (Contains(index))
+                    Remove(index);
+                Add(index, value);
+            }
+        }
+
+        public T1 this[T2 index]
+        {
+            get { return backward[index]; }
+            set
+            {
+                if (Contains(value)) throw new ArgumentException("The given value is already in the dictionary; TwoWayDictionary allows only 1:1 connections, therefore each value may only be added once, whether as key or as value");
+                if (Contains(index))
+                    Remove(index);
+                Add(value, index);
+            }
+        }
+
+        public void Add(T1 t1, T2 t2)
+        {
+            if (t1 == null || t2 == null) throw new ArgumentNullException();
+            if (forward.ContainsKey(t1)) throw new ArgumentException("Already contains key " + t1.ToString());
+            if (backward.ContainsKey(t2)) throw new ArgumentException("Already contains key " + t2.ToString());
+
+            forward.Add(t1, t2);
+            backward.Add(t2, t1);
+        }
+
+        public void Remove(T1 t1)
+        {
+            if (t1 == null) throw new ArgumentNullException();
+
+            T2 t2 = forward[t1];
+            forward.Remove(t1);
+            backward.Remove(t2);
+        }
+
+        public void Remove(T2 t2)
+        {
+            if (t2 == null) throw new ArgumentNullException();
+
+            T1 t1 = backward[t2];
+            backward.Remove(t2);
+            forward.Remove(t1);
+        }
+
+        public void Overwrite(T1 key, T2 newValue, T2 oldValue)
+        {
+            if (!forward[key].Equals(oldValue)) throw new ArgumentException("The given key and oldValue do not correspond");
+            if (backward.ContainsKey(newValue)) throw new ArgumentException("The given newValue is already in the dictionary");
+
+            Remove(key);
+            Add(key, newValue);
+        }
+
+        public void Overwrite(T2 key, T1 newValue, T1 oldValue)
+        {
+            if (!backward[key].Equals(oldValue)) throw new ArgumentException("The given key and oldValue do not correspond");
+            if (forward.ContainsKey(newValue)) throw new ArgumentException("The given newValue is already in the dictionary");
+
+            Remove(key);
+            Add(newValue, key);
+        }
+
+        // Allows for exposing more restricted access; essentially normal read-only dictionary
+        public class Exposer<T3, T4>
+        {
+            private readonly Dictionary<T3, T4> dict;
+
+            public Exposer(Dictionary<T3, T4> dict)
+            {
+                this.dict = dict;
+            }
+
+            public T4 this[T3 index]
+            {
+                get { return dict[index]; }
+            }
+
+            public int Count => dict.Count;
+
+            public IReadOnlyCollection<T3> Keys => dict.Keys;
+            public IReadOnlyCollection<T4> Values => dict.Values;
+
+            public bool ContainsKey(T3 key)
+            {
+                return dict.ContainsKey(key);
+            }
+
+            public bool ContainsValue(T4 value)
+            {
+                return dict.ContainsValue(value);
+            }
+        }
+    }
+
+}
